c7a286263a8a8864a8c3f1868ff3ea08f7efb7f1
ChristianBenedictEduvas <105403686+ChristianBenedictEduvas@users.noreply.github.com>
2022-05-31 00:19:40 +0800

    FINAL STOPWATCH

--- Stopwatch.py
+++ Stopwatch.py
@@ -1,13 +1,8 @@
-# varibales are milliseconds, seconds, minutes, and hours
-# needs start, stop, split, and reset
-# can add other features 
-# we'll utilize Tkinter for import to run GUI
-
 import tkinter as tk
-import time 
 
 running = False
 hours, minutes, seconds, milliseconds = 0, 0, 0, 0
+laps = 0
 
 def start():
     global running
@@ -20,6 +15,7 @@ def pause():
     if running:
         stopwatch_label.after_cancel(update_time)
         running = False
+        
 def reset():
     global running
     if running:
@@ -27,12 +23,50 @@ def reset():
         running = False
 
     global hours, minutes, seconds, milliseconds
+    global laps
     hours, minutes, seconds, milliseconds = 0, 0, 0, 0
-    stopwatch_label.configure(text='00:00:00:00')
+    laps = 0
+    stopwatch_label.config(text='00:00:00.00')
+    split1.configure(text='00:00:00.00')
+    split2.configure(text='00:00:00.00')
+    split3.configure(text='00:00:00.00')
+    split4.configure(text='00:00:00.00')
+    split5.configure(text='00:00:00.00')
+    split6.configure(text='00:00:00.00')
+    split7.configure(text='00:00:00.00')
+    split8.configure(text='00:00:00.00')
+    split9.configure(text='00:00:00.00')
+    split10.configure(text='00:00:00.00') 
+    stopwatch_label.configure(text='00:00:00.00')
+
+def split():
+    global running
+    if running:
+        global laps
+        laps += 1
+        if laps == 1:
+            split1.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 2:
+            split2.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 3:
+            split3.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 4:
+            split4.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 5:
+            split5.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 6:
+            split6.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 7:
+            split7.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 8:
+            split8.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 9:
+            split9.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
+        elif laps == 10:
+            split10.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
 
 def update():
     global hours, minutes, seconds, milliseconds
-    time.sleep(0.0075)
     milliseconds += 1
     if milliseconds == 66:
         seconds += 1
@@ -44,32 +78,57 @@ def update():
         hours += 1
         minutes = 0
     
-    
-    hours_string = f'{hours}' if hours > 9 else f'0{hours}'
-    minutes_string = f'{minutes}' if minutes > 9 else f'0{minutes}'
-    seconds_string =f'{seconds}' if seconds > 9 else f'0{seconds}'
-    milliseconds_string =f'{milliseconds}' if milliseconds > 9 else f'0{milliseconds}'
-    stopwatch_label.config(text=hours_string + ':' + minutes_string + ':' + seconds_string + '.' + milliseconds_string)
+    global hours_split, minutes_split, seconds_split, milliseconds_split
+    hours_split = f'{hours}' if hours > 9 else f'0{hours}'
+    minutes_split = f'{minutes}' if minutes > 9 else f'0{minutes}'
+    seconds_split =f'{seconds}' if seconds > 9 else f'0{seconds}'
+    milliseconds_split =f'{milliseconds}' if milliseconds > 9 else f'0{milliseconds}'
+    stopwatch_label.config(text=hours_split + ':' + minutes_split + ':' + seconds_split + '.' + milliseconds_split)
     global update_time
-    update_time = stopwatch_label.after(1, update)
+    update_time = stopwatch_label.after(10, update)
 
 
 root = tk.Tk()
 root.geometry('630x800')
 root.title('Stopwatch')
-root.configure(bg="white")
+root.configure(bg='black')
 
-stopwatch_label = tk.Label(text='00:00:00:00', font=('Arial', 80, 'bold'))
+stopwatch_label = tk.Label(text='00:00:00.00', font=('Arial', 86, 'bold'))
 stopwatch_label.pack()
 
+# label to display time
 
-start_button = tk.Button(text='Start', height=5, width=7, font=('Arial', 20, 'bold',), borderwidth='5', command=Start)
+# Laps
+split1 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split1.pack()
+split2 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split2.pack()
+split3 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split3.pack()
+split4 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split4.pack()
+split5 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split5.pack()
+split6 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split6.pack()
+split7 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split7.pack()
+split8 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split8.pack()
+split9 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split9.pack()
+split10 = tk.Label(text="00:00:00.00", font=("Arial", 25, 'bold'), height="1")
+split10.pack()
+
+start_button = tk.Button(text='Start', height=5, width=7, font=('Arial', 20, 'bold',), borderwidth='5', command=start)
 start_button.pack(side=tk.LEFT)
-pause_button = tk.Button(text='Pause', height=5, width=7, font=('Arial', 20, 'bold'), borderwidth='5', command=Pause)
+split_button = tk.Button(text='Split', height=5, width=7, font=('Arial', 20, 'bold'), borderwidth='5', command=split)
+split_button.pack(side=tk.LEFT)
+pause_button = tk.Button(text='Stop', height=5, width=7, font=('Arial', 20, 'bold'), borderwidth='5', command=pause)
 pause_button.pack(side=tk.LEFT)
-reset_button = tk.Button(text='Reset', height=5, width=7, font=('Arial', 20, 'bold'), borderwidth='5', command=Reset)
+reset_button = tk.Button(text='Reset', height=5, width=7, font=('Arial', 20, 'bold'), borderwidth='5', command=reset)
 reset_button.pack(side=tk.LEFT)
-quit_button = tk.Button(text='Quit', height=5, width=7, font=('Arial', 20, 'bold'), borderwidth='5', command=Quit)
+quit_button = tk.Button(text='Exit', height=5, width=7, font=('Arial', 20, 'bold'), borderwidth='5', command=quit)
 quit_button.pack(side=tk.LEFT)
 
 
