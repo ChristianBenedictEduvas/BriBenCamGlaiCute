02c7cdfa1bc72114a223d438957cbc3aa05405a2
Glaiza Anne G. Quillo <105399010+GlaizaAnneQuillo@users.noreply.github.com>
2022-05-30 07:34:05 -0800

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-def start():
-    global running
-    if not running:
-        update()
-        running = True
-
-start_button = tk.Button(text='Start', height=5, width=7, font=('Arial', 20, 'bold',), borderwidth='5', command=start)
-start_button.pack(side=tk.LEFT)
