cb66ab6c0107440641b255fbd9afe105cc2ce84a
ChristianBenedictEduvas <105403686+ChristianBenedictEduvas@users.noreply.github.com>
2022-05-30 22:29:02 +0800

    Stopwatch

--- main variables.py
+++ main variables.py
@@ -1,3 +1,17 @@
+import tkinter as tk
+
 running = False
 hours, minutes, seconds, milliseconds = 0, 0, 0, 0
 laps = 0
+def update():
+    global hours, minutes, seconds, milliseconds
+    milliseconds += 1
+    if milliseconds == 66:
+        seconds += 1
+        milliseconds = 0
+    if seconds == 60:
+        minutes += 1
+        seconds = 0
+    if minutes == 60:
+        hours += 1
+        minutes = 0
