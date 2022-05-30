6ae49b730e1311da333d161a1c8ab0f49da5fcd2
Estores, Camille <105400451+Estores@users.noreply.github.com>
2022-05-31 00:46:36 +0800

    Create Stopwatch.py

--- Stopwatch.py
+++ Stopwatch.py
@@ -15,13 +15,12 @@ def pause():
     if running:
         stopwatch_label.after_cancel(update_time)
         running = False
-     
-    def reset():
+        
+def reset():
     global running
     if running:
         stopwatch_label.after_cancel(update_time)
         running = False
-        
 
     global hours, minutes, seconds, milliseconds
     global laps
@@ -40,55 +39,8 @@ def pause():
     split10.configure(text='00:00:00.00') 
     stopwatch_label.configure(text='00:00:00.00')
 
-def split():
 
-    global running
 
-    if running:
-
-        global laps
-
-        laps += 1
-
-        if laps == 1:
-
-            split1.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 2:
-
-            split2.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 3:
-
-            split3.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 4:
-
-            split4.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 5:
-
-            split5.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 6:
-
-            split6.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 7:
-
-            split7.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 8:
-
-            split8.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 9:
-
-            split9.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
-
-        elif laps == 10:
-
-            split10.configure(text = hours_split + ':' + minutes_split + ':' + seconds_split + "." + milliseconds_split)
 
 
 def split():
