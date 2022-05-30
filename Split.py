8c5766dba32f7b2f1850569b554e893f48f2a248
Estores, Camille <105400451+Estores@users.noreply.github.com>
2022-05-30 23:23:29 +0800

    Create Split.py

---
+++ Split.py
@@ -0,0 +1,32 @@
+global hours, minutes, seconds, milliseconds
+
+    global laps
+
+    hours, minutes, seconds, milliseconds = 0, 0, 0, 0
+
+    laps = 0
+
+    stopwatch_label.config(text='00:00:00.00')
+
+    split1.configure(text='00:00:00.00')
+
+    split2.configure(text='00:00:00.00')
+
+    split3.configure(text='00:00:00.00')
+
+    split4.configure(text='00:00:00.00')
+
+    split5.configure(text='00:00:00.00')
+
+    split6.configure(text='00:00:00.00')
+
+    split7.configure(text='00:00:00.00')
+
+    split8.configure(text='00:00:00.00')
+
+    split9.configure(text='00:00:00.00')
+
+    split10.configure(text='00:00:00.00') 
+
+    stopwatch_label.configure(text='00:00:00.00')
+
